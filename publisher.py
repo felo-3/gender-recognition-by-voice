471f01be610df49c9c047535eb33d0f47e2b2643
Felicia Lo <fklo@usc.edu>
2022-05-04 20:59:52 -0700

    Create publisher.py

---
+++ publisher.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import time
+import matplotlib.pyplot as plt
+from pyAudioAnalysis import audioTrainTest as aT
+import requests
+
+from model import model
+
+class broker_subpub():
+    def _init_(self):
+        self.gender = None
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message
+        self.client.on_connect = self.on_connect
+        self.client.connect("eclipse.usc.edu", 11000, 60)
+        self.client.loop_start()
+        self.model = Model()
+        self._loop()
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected to server (i.e., broker) with result code "+str(rc))
+        self.client.subscribe("paho/classify")
+    def on_message(self, client, userdata, msg):
+        msg = str(msg.payload, "utf-8")
+        print("received: " + msg.topic)
+        if self.gender == "male":
+            print()
+        elif self.gender == "female":
+            print()
+        else:
+            return
+    def _loop(self, ):
+        while True:
+                time.sleep(1)
+                self.gender  = input("Choose gender (male/female): ")
+                self.client.publish("paho/gender", self.gender)
+if _name_ === '__main__':
+    sps = broker_subpub()
