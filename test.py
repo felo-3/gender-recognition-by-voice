aaa535c02a76f7e0d50769540895b6011fe91470
Felicia Lo <fklo@usc.edu>
2022-05-05 19:46:28 -0700

    Update test.py

--- test.py
+++ test.py
@@ -175,6 +175,7 @@ def get_gender(filename):
     model = create_model()
     # load the saved/trained weights
     model.load_weights("results/model.h5")
+    """
     if not file or not os.path.isfile(file):
         # if file not provided, or it doesn't exist, use your voice
         print("Please talk")
@@ -182,6 +183,7 @@ def get_gender(filename):
         file = "test-mic.wav"
         # record the file (start talking)
         record_to_file(file)
+    """
     # extract features and reshape it
     features = extract_feature(file, mel=True).reshape(1, -1)
     # predict the gender!
